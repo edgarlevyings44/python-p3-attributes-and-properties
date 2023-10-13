63f5478c844453d1a91bb6d12040691a519f4ce7
Levy <edgarlevy81@gmail.com>
2023-10-13 23:44:34 +0300

    Lab attempted

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,33 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+
+    def __init__(self, name, breed):
+        self.set_name(name)
+        self.set_breed(breed)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 25 characters.")
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, value):
+        if value in self.APPROVED_BREEDS:
+            self._breed = value
+        else:
+            raise ValueError("Breed must be in the list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    APPROVED_JOBS = [
+        "Admin",
+        "Customer Service",
+        "Human Resources",
+        "ITC",
+        "Production",
+        "Legal",
+        "Finance",
+        "Sales",
+        "General Management",
+        "Research & Development",
+        "Marketing",
+        "Purchasing"
+    ]
+
+    def __init__(self, name, job):
+        self.set_name(name)
+        self.set_job(job)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, value):
+        if value in self.APPROVED_JOBS:
+            self._job = value
+        else:
+            raise ValueError("Job must be in the list of approved jobs.")
